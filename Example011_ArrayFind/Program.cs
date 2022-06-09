e9ed998f82f1b40a94f30e2f0888f3ccd9f04b82
Sergey <metrolog_omsk@mail.ru>
2022-06-09 19:08:37 +0500

    Решили задачу с массивами и циклом

---
+++ Example011_ArrayFind/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {15, 221, 37, 14, 25, 56, 17, 14, 18, 19};
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //выйти из цикла
+    }
+    index++;
+}
